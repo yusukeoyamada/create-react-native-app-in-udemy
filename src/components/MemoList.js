c01e9690b4be4e3765069e237437ef36f8700e3c
yusukeoyamada <fa19940118@gmail.com>
2019-09-06 16:51:09 +0900

    Memo一覧機能実装

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
 class MemoList extends Component {
+  renderMemo({item}) {
+    return (
+      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>{item.body}</Text>
+          <Text style={styles.memoDate}>2019/09/04</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
       <View style={styles.memoList}>
-        <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2019/09/04</Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2019/09/04</Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2019/09/04</Text>
-          </View>
-        </TouchableHighlight>
+        <FlatList
+          data={this.props.memoList}
+          renderItem={this.renderMemo.bind(this)}
+        />
       </View>
     );
   }
--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      memoList: [],
+    }
+  }
+
+  componentWillMount(){
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    db.collection(`users/${currentUser.uid}/memos`)
+      .get()
+      .then(snapshot => {
+        let memoList = [];
+        snapshot.forEach(doc => {
+          memoList.push({ ...doc.data(), key: doc.id });
+        });
+        this.setState({ memoList });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   handlePress(){
     const { navigation } = this.props;
     navigation.navigate('MemoCreate');
@@ -13,7 +39,7 @@ class MemoListScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MemoList navigation={this.props.navigation} />
+        <MemoList memoList={this.state.memoList} navigation={this.props.navigation} />
         <CircleButton name="plus" onPress={this.handlePress.bind(this)} />
       </View>
     );
